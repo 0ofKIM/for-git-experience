da16586b3dee1f76316b4ca480a542d33687eb97
uykim3 <uykim3@lotte.net>
2019-07-20 17:10:16 +0900

    second commit

---
+++ ForGitExperience/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForGitExperience
+//
+//  Created by Lotte on 20/07/2019.
+//  Copyright © 2019 uykim3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
