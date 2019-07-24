89e697094392ac0b46e4993461552412322e84ce
uykim3 <uykim3@lotte.net>
2019-07-24 17:40:31 +0900

    2019/07/24 - Delegate를 이용한 Data 전달

---
+++ ForGitExperience/DelegateDataReceiveViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DelegateDataViewController.swift
+//  ForGitExperience
+//
+//  Created by Lotte on 23/07/2019.
+//  Copyright © 2019 uykim3. All rights reserved.
+//
+
+import UIKit
+
+// Delegate를 이용한 ViewController간 Data전달방법
+// sendDataDelegate (1.채택)
+class DelegateDataReceiveViewController: UIViewController {
+    
+    @IBOutlet var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "show" {
+            let viewController : DelegateDataSendViewController = segue.destination as! DelegateDataSendViewController
+            viewController.delegate = self
+        }
+    }
+}
+
+//convention에 맞게 extension으로 빼줌
+extension DelegateDataReceiveViewController: SendDataDelegate {
+    //옵셔널로 선언한게 아니었으므로 반드시 선언(2.준수)
+    func sendData(data: String) {
+        myLabel.text = data
+    }
+}
