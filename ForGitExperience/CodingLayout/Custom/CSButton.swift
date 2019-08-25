1d5e79d88a17bdaaa23e3f260d0065fb86e4b237
uykim3 <uykim3@lotte.net>
2019-08-25 19:04:35 +0900

    2019/08/25-Custom

--- ForGitExperience/CodingLayout/Custom/CSButton.swift
+++ ForGitExperience/CodingLayout/Custom/CSButton.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+// 버튼 타입을 결정하는 열거형
+public enum CSButtonType {
+    case rect
+    case circle
+}
+
 class CSButton: UIButton {
 
     required init?(coder aDecoder: NSCoder) {
@@ -33,4 +39,52 @@ class CSButton: UIButton {
         super.init(frame: CGRect.zero)
     }
 
+    convenience init(type: CSButtonType) {
+        self.init()
+
+        switch type {
+        case .rect :
+            self.backgroundColor = UIColor.black // 배경을 검은색으로
+            self.layer.borderColor = UIColor.black.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 0 // 모서리는 전혀 둥글지 않게
+            self.setTitleColor(UIColor.white, for: .normal) // 글씨는 흰색으로
+            self.setTitle("Rect Button", for: .normal) // 기본 문구 설정
+        case .circle :
+            self.backgroundColor = UIColor.red // 배경을 Qkfrks색으로
+            self.layer.borderColor = UIColor.black.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 50 // 모서리는 전혀 둥글지 않게
+            self.setTitle("Circle Button", for: .normal) // 기본 문구 설정
+
+        }
+
+        self.addTarget(self, action: #selector(counting(_:)), for: .touchUpInside)
+    }
+
+    var style: CSButtonType = .rect {
+        didSet {
+            switch style {
+            case .rect :
+                self.backgroundColor = UIColor.black // 배경을 검은색으로
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0 // 모서리는 전혀 둥글지 않게
+                self.setTitleColor(UIColor.white, for: .normal) // 글씨는 흰색으로
+                self.setTitle("Rect Button", for: .normal) // 기본 문구 설정
+            case .circle :
+                self.backgroundColor = UIColor.red // 배경을 Qkfrks색으로
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 50 // 모서리는 전혀 둥글지 않게
+                self.setTitle("Circle Button", for: .normal) // 기본 문구 설정
+            }
+        }
+    }
+
+    // 버튼이 클릭된 횟수를 카운트하여 타이틀에 표시해 주는 함수
+    @objc func counting(_ sender: UIButton) {
+        sender.tag = sender.tag + 1
+        sender.setTitle("\(sender.tag) 번째 클릭", for: .normal)
+    }
 }
