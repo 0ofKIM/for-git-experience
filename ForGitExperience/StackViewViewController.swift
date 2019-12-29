787571ad59716f22734dee68429a1d10f0b5699b
0ofKim <>
2019-12-30 02:03:52 +0900

    2019.12.29 - 가변 높이 테스트

--- ForGitExperience/StackViewViewController.swift
+++ ForGitExperience/StackViewViewController.swift
@@ -15,7 +15,7 @@ class StackViewViewController: UIViewController {
     func insertLabel(stackView: UIStackView, at: Int) {
         var firstLabel: UILabel!
         firstLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 10))
-        firstLabel.text = "테스트\(at)"
+        firstLabel.text = "가변 높이 테스트\(at)"
 
         stackView.spacing = 40
         stackView.insertArrangedSubview(firstLabel, at: at)
@@ -25,8 +25,8 @@ class StackViewViewController: UIViewController {
         super.viewDidLoad()
 
         insertLabel(stackView: self.stackView, at: 0)
-//        insertLabel(stackView: self.stackView, at: 1)
-//        insertLabel(stackView: self.stackView, at: 2)
+        insertLabel(stackView: self.stackView, at: 1)
+        insertLabel(stackView: self.stackView, at: 2)
 //        insertLabel(stackView: self.stackView, at: 3)
 //        insertLabel(stackView: self.stackView, at: 4)
     }
