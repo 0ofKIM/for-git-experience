c2724defe1bebe0d18542fc1fd43c2e85bd9226f
0ofKim <>
2020-01-12 23:53:33 +0900

    2020.01.12 - 라벨 가변 길이 테스트

---
+++ ForGitExperience/LabelTestViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LabelTestViewController.swift
+//  ForGitExperience
+//
+//  Created by 0ofKim on 2020/01/12.
+//  Copyright © 2020 uykim3. All rights reserved.
+//
+
+import UIKit
+
+class LabelTestViewController: UIViewController {
+
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        testLabel.text = """
+//        첫줄
+//        두줄
+//        세줄
+//        네줄
+//        """
+        testLabel.text = "welnflwnfljwnfjlwnfjlawnekfjnwekjfnweifbwijfbhiwbfiwbfiwebfuiwbiufbwiufbaiwuebfiuwebfiuwbfiuewbifuwbeiufbwiufbawieufbi"
+    }
+    
+}
